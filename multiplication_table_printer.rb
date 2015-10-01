2d892905251e7a7f72db98879ca7b27f32a6b8da
Stefan Kojic <stefanste@gmail.com>
2015-10-01 18:48:57 +0100

    Add printing of multiplication rows + spec

--- multiplication_table_printer.rb
+++ multiplication_table_printer.rb
@@ -1,22 +1,32 @@
 class MultiplicationTablePrinter
   def initialize(prime_numbers)
     @prime_numbers = prime_numbers
-    @column_width = column_width
+    @column_width = column_width # Determine column width for nicely aligned table
   end
 
   def print_table
     puts header_row
     puts header_row.size.times.map { '-' }.join
-    #print_multiplication_rows
+    multiplication_rows.each { |row| puts row }
   end
 
   private
+
+  def multiplication_rows
+    @prime_numbers.map do |current_num|
+      row_string = ''
+      row_string << formatted_number_string(current_num)
+      row_string << '|'
+      @prime_numbers.each { |prime_num| row_string << formatted_number_string(current_num * prime_num) }
+      row_string
+    end
+  end
 
   def header_row
     header_row_string = ''
     @column_width.times { header_row_string << ' ' }
     header_row_string << '|'
-    @prime_numbers.each { |num| header_row_string << number_string(num) }
+    @prime_numbers.each { |num| header_row_string << formatted_number_string(num) }
     header_row_string
   end
 
@@ -25,7 +35,7 @@ class MultiplicationTablePrinter
     highest_product.to_s.size + 1 # +1 for spacing
   end
 
-  def number_string(number)
+  def formatted_number_string(number)
     number_of_spaces = @column_width - number.to_s.size
     (number_of_spaces.times.map { ' ' } + [number.to_s]).join
   end
--- spec/multiplication_table_printer_spec.rb
+++ spec/multiplication_table_printer_spec.rb
@@ -2,9 +2,21 @@ require_relative '../multiplication_table_printer'
 
 describe MultiplicationTablePrinter do
   context "Printing out the multiplication table" do
-    let(:table_printer) { MultiplicationTablePrinter.new([2, 3, 5]) }
-    specify do
-      expect(table_printer.send(:header_row)).to eq "   |  2  3  5"
+    let(:table_printer) { MultiplicationTablePrinter.new([2, 3, 5, 7, 11]) }
+
+    context "Printing the header row" do
+      specify do
+        expect(table_printer.send(:header_row)).to eq "    |   2   3   5   7  11"
+      end
     end
+
+    context "Printing the multiplication rows" do
+      specify do
+        expect(table_printer.send(:multiplication_rows)[0]).to eq "   2|   4   6  10  14  22"
+        expect(table_printer.send(:multiplication_rows)[2]).to eq "   5|  10  15  25  35  55"
+        expect(table_printer.send(:multiplication_rows)[4]).to eq "  11|  22  33  55  77 121"
+      end
+    end
+
   end
 end
